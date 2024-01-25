9d4d7a6ebff28bebd5b4eb592fa8e07114b35798
Quoc Nguyen <nguyen95688@gmail.com>
2024-01-25 14:25:55 +0700

    add nhom 11

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'dart:html';
+
+
+import 'package:flutter/material.dart';
+
+import 'Index-1.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Index(),
+      ),
+    );
+  }
+}
+
